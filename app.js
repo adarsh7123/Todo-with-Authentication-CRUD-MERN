7b165b75075443b737c3dc79a6625b98eaaadb2f
adarsh7123 <adarshverma127@gmail.com>
2024-05-17 14:03:32 +0530

    cors added

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
+
 import express from "express";
-import userRouter from "./routes/user.js"
-import taskRouter from "./routes/task.js"
+import userRouter from "./routes/user.js";
+import taskRouter from "./routes/task.js";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors";
 
 export const app = express();
 
 config({
-  path: "./data/config.env"
-})
+  path: "./data/config.env",
+});
 
-//using middleware
+// Using Middlewares
 app.use(express.json());
 app.use(cookieParser());
-//using routes
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+// Using routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/task", taskRouter);
 
-
-app.get('/', (req, res) => {
-  res.send('nice work');
+app.get("/", (req, res) => {
+  res.send("Nice working");
 });
 
-
-//using error middleware
+// Using Error Middleware
 app.use(errorMiddleware);
-
-
